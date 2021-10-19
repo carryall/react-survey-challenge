33f92cd2083534a3ed2db1a651e526126858b1cc
Carryall <carryall369@gmail.com>
2021-10-19 15:27:08 +0700

    [#4] Add base font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        gray: {
+          100: '#f9fbfd',
+          200: '#eef2f5',
+          300: '#e6e9ed',
+          400: '#c3c6ce',
+          500: '#9ea1ac',
+          600: '#747784',
+          700: '#4d505b',
+          800: '#3b3d47',
+          900: '#292a33'
+        },
+        blue: '#0041e2',
+        red: '#e20041',
+        black: '#15151a'
+      }
+    }
   },
   variants: {
     extend: {}
